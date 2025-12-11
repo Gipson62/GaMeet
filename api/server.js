28b5fd763239eeff84deeb13ecfb3033933576ee
Léo Schoofs <leo.schoofs5@gmail.com>
2025-12-11 16:42:59 +0100

    Cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {default as Router} from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(Router);
 
