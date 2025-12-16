9305f7f5f9d126156444c73f839db79efa949f65
Léo Schoofs <leo.schoofs5@gmail.com>
2025-12-16 16:04:47 +0100

    Router fix

--- website/src/services/Router.jsx
+++ website/src/services/Router.jsx
@@ -1,12 +1,10 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
-import App from '../App.jsx'; // On importe le cadre
 import Login from '../pages/Login.jsx';
 import Event from '../pages/Event.jsx';
-// import UserProfile from '../pages/UserProfile';
 
 const router = createBrowserRouter([
     {
-        path: "/", // Redirection par défaut
+        path: "/",
         element: <Navigate to="/login" replace />
     },
     {
@@ -14,7 +12,7 @@ const router = createBrowserRouter([
         element: <Login />
     },
     {
-        path: "/event", // Redirection par défaut
+        path: "/event",
         element: <Event />
     },
 ]);
