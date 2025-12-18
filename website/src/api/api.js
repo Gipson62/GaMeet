572a598bf09200ed5acd67cca3620cb08294f7e3
Gipson62 <70176484+Gipson62@users.noreply.github.com>
2025-12-18 20:17:19 +0100

    Removed shitty vscode git stuff

--- website/src/api/api.js
+++ website/src/api/api.js
@@ -357,10 +357,6 @@ export const removeParticipant = async (eventId, userId, token) => {
 
   return true;
 };
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 export const deleteReview  = async (reviewId, token) => {
   const res = await fetch(`http://localhost:3001/v1/review/${reviewId}`, {
