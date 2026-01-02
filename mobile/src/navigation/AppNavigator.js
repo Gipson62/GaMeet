f69c2059dec21b26891a562553e6f3f41b5a3de2
Roipopof <leseure2006@gmail.com>
2026-01-02 13:54:38 +0100

    barre navigation

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -1,1 +1,69 @@
-//barre de navigation
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+
+// Import des écrans
+import Login from '../screens/Login';
+import Register from '../screens/Register';
+import Home from '../screens/Home';
+import GameList from '../screens/GameList';
+import EventList from '../screens/EventList';
+import Map from '../screens/Map';
+import Profile from '../screens/Profile';
+import GameDetails from '../screens/GameDetails';
+import EventDetails from '../screens/EventDetails';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Event"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Accueil') {
+            iconName = 'home';
+          } else if (route.name === 'Jeux') {
+            iconName = 'sports-esports';
+          } else if (route.name === 'Event') {
+            iconName = 'event';
+          } else if (route.name === 'Carte') {
+            iconName = 'map';
+          } else if (route.name === 'Profile') {
+            iconName = 'person';
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#6200ee',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Accueil" component={Home} />
+      <Tab.Screen name="Jeux" component={GameList} />
+      <Tab.Screen name="Event" component={EventList} />
+      <Tab.Screen name="Carte" component={Map} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={HomeTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="GameDetails" component={GameDetails} />
+        <Stack.Screen name="EventDetails" component={EventDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
